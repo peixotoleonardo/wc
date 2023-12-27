d6e86b35ea76211432e806367ea0a6521c58a721
Leonardo Peixoto <leonardo.peixoto@fortics.com.br>
2023-12-27 12:33:16 -0300

    feat: add option to count characters

--- src/option-loader.mjs
+++ src/option-loader.mjs
@@ -1,4 +1,5 @@
 import { CountBytesOption } from './options/count-bytes.option.mjs';
+import { CountCharactersOption } from './options/count-characters.option.mjs';
 import { CountLinesOption } from './options/count-lines.option.mjs';
 import { CountWordsOption } from './options/count-words.option.mjs';
 
@@ -10,5 +11,6 @@ export class OptionLoader {
     new CountBytesOption().load(program);
     new CountLinesOption().load(program);
     new CountWordsOption().load(program);
+    new CountCharactersOption().load(program);
   }
 }
