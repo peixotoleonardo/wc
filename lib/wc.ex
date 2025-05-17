c527a1f2327ed2315a2b47f777f27158bff55d57
Leonardo Peixoto <leonardo.peixoto@fortics.com.br>
2025-05-17 13:43:38 -0300

    chore: init elixir project

---
+++ lib/wc.ex
@@ -0,0 +1,18 @@
+defmodule Wc do
+  @moduledoc """
+  Documentation for `Wc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Wc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
